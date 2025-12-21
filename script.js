a08e93499329165b4d40be1231d3ce123490a03f
arbitrary142857 <jmao142857@gmail.com>
2025-12-21 02:30:34 -0500

    Progress 2

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+const btn = document.querySelector("button")
+btn.addEventListener("click", clickButton)
+
+function clickButton(event) {
+    const height = Number(document.getElementById("height").value)
+    const width = Number(document.getElementById("width").value)
+
+    if (Number.isInteger(height) && height >= 1 && height <= 10 &&
+        Number.isInteger(width) && width >= 1 && width <= 10) {
+            event.preventDefault()
+            removeWarning()
+            let grid = createGrid(height, width)
+            drawGrid(grid)
+    }
+    else {
+        addWarning()
+    }
+}
+
+function removeWarning() {
+    const form = document.querySelector("form")
+    if (form.querySelector(".warning-text")) {
+        form.removeChild(form.querySelector(".warning-text"))
+    }
+}
+
+function addWarning() {
+    const form = document.querySelector("form")
+    if (!form.querySelector(".warning-text")) {
+        const warning = document.createElement("p")
+        warning.innerText = "Grid height and width must be positive integers less than or equal to 10."
+        warning.classList.add("warning-text")
+        form.appendChild(warning)
+    }
+}
+
 function createGrid(height, width) {
     let grid = []
     for (let i = 0; i < height; i++) {
@@ -11,7 +47,35 @@ function createGrid(height, width) {
 }
 
 function drawGrid(grid) {
-    pass
+    console.log("drawing grid...")
+
+    // Removing current lights...
+    const lightGrid = document.querySelector(".wrapper")
+    let removedLight = lightGrid.querySelector("img")
+    while (removedLight) {
+        lightGrid.removeChild(removedLight)
+        removedLight = lightGrid.querySelector("img")
+    }
+
+    // Adding new lights....
+    lightGrid.style.setProperty("--columns", grid[0].length)
+    lightGrid.style.setProperty("--rows", grid.length)
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            const addedLight = document.createElement("img")
+            if (grid[i][j]) {
+                addedLight.src = "images/on-light.png"
+                addedLight.alt = "on"
+                addedLight.classList.add("light")
+            }
+            else {
+                addedLight.src = "images/off-light.png"
+                addedLight.alt = "off"
+                addedLight.classList.add("light")
+            }
+            lightGrid.appendChild(addedLight)
+        }
+    }
 }
 
 function updateGrid(grid, row, col) {
